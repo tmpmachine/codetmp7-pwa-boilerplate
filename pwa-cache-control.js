0baab73109e2ba7a2b6556bc6502a1e066dce4ff
tmpmachine <bill.yosafatz@gmail.com>
2023-05-24 07:02:53 +0700

    Create pwa-cache-control.js

---
+++ pwa-cache-control.js
@@ -0,0 +1,60 @@
+window.pwaCache = (function() {
+  
+  // change this with your unique cache name
+  let cacheName = 'sampleApp-NzY3OTc5OA';
+
+  let SELF = {};
+  
+  function extractUrlsFromJson(json) {
+    let urls = [];
+    for (let key in json) {
+      if (Array.isArray(json[key])) {
+        urls = urls.concat(json[key]);
+      }
+    }
+    return urls;
+  }
+    
+  SELF.uncache = function() {
+    
+    caches.delete(cacheName)
+      .then(() => {
+          alert('Done! Reload to take effect.');
+      });
+  };
+  
+  SELF.recache = function() {
+    
+    fetch('manifest-cache.json')
+    .then(res => res.json())
+    .then(json => {
+      
+      let cacheURLs = extractUrlsFromJson(json);
+  
+      caches.delete(cacheName)
+      .then(() => {
+        caches.open(cacheName)
+        .then(function(cache) {
+          return Promise.all(
+            cacheURLs.map(function(url) {
+              return cache.add(url).catch(function(error) {
+                console.error('Failed to cache URL:', url, error);
+              });
+            })
+          );
+        })
+        .then(function() {
+          alert('Done! Reload to take effect.');
+        })
+        .catch(function(error) {
+          alert('Failed. Check console.');
+        });
+      });
+      
+    
+    });
+  };
+  
+  return SELF; 
+  
+})();
